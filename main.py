306a74e82670b28a84b63f212d941a6e887d5c65
Ron Houk <ron.houk@leblibrary.com>
2023-10-29 14:13:28 -0400

    removed commented lines

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ r = requests.post('https://leblibrary.libcal.com/1.1/oauth/token',
                         "client_secret": config['api-token'],
                         "grant_type": "client_credentials"})
 
-
 access_token = r.json().get('access_token')
 
 events_headers = {'Content-type': 'application/json', 'Accept': 'text/plain', 'Authorization': f"Bearer {access_token}"}
@@ -33,21 +32,8 @@ leb_events = requests.get('https://leblibrary.libcal.com/1.1/events?cal_id=17790
 
 
 unsorted_events = kilton_events['events'] + leb_events['events']
-print(unsorted_events[0]["start"])
 
 Event.add_events(unsorted_events)
-# for event in unsorted_events:
-#     an_event = Event(event)
-#     # print(an_event, len(an_event))
-#     Event.add_event(an_event)
-#     # print(Event.length())
-
-
-# Event.vertical_chain()
-# Event.horizontal_chain()
-# Event.ring_twist()
-# Event.ring_around()
-# Event.clusters()
 Event.arrange()
 
 output = template.render({"events": Event.list_events()})
